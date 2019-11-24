fb5b1b50b3ceca35110d3e98ca290e85faa1009d
Elias Bernardo <ebmm01@gmail.com>
2019-11-24 16:01:42 -0300

    Implementing movie-last-updated-migration and finishing Chapter 3

--- src/migrations/movie-last-updated-migration.js
+++ src/migrations/movie-last-updated-migration.js
@@ -1,6 +1,7 @@
 const MongoClient = require("mongodb").MongoClient
 const ObjectId = require("mongodb").ObjectId
 const MongoError = require("mongodb").MongoError
+require("dotenv").config()
 
 /**
  * Ticket: Migration
@@ -17,23 +18,19 @@ const MongoError = require("mongodb").MongoError
 ;(async () => {
   try {
     // ensure you update your host information below!
-    const host = "mongodb://<your atlas connection uri from your .env file"
-    const client = await MongoClient.connect(
-      host,
-      { useNewUrlParser: true },
-    )
+    const host = `${process.env.MFLIX_DB_URI}`
+    const client = await MongoClient.connect(host, { useNewUrlParser: true })
     const mflix = client.db(process.env.MFLIX_NS)
-
     // TODO: Create the proper predicate and projection
     // add a predicate that checks that the `lastupdated` field exists, and then
     // check that its type is a string
     // a projection is not required, but may help reduce the amount of data sent
     // over the wire!
-    const predicate = { somefield: { $someOperator: true } }
+    const predicate = { lastupdated: { $exists: true, $type: "string" } }
     const projection = {}
     const cursor = await mflix
       .collection("movies")
-      .find(predicate, projection)
+      .find(predicate)
       .toArray()
     const moviesToMigrate = cursor.map(({ _id, lastupdated }) => ({
       updateOne: {
@@ -48,7 +45,9 @@ const MongoError = require("mongodb").MongoError
       `Found ${moviesToMigrate.length} documents to update`,
     )
     // TODO: Complete the BulkWrite statement below
-    const { modifiedCount } = await "some bulk operation"
+    const { modifiedCount } = await mflix
+      .collection("movies")
+      .bulkWrite(moviesToMigrate)
 
     console.log("\x1b[32m", `${modifiedCount} documents updated`)
     client.close()
